0e9e8c3a978b54c13664571c79751cbcdd2d1149
John Omar Esguerra <esguerrajohnomar@gmail.com>
2020-07-06 12:52:25 +0800

    Initial commit

--- cropmap/settings.py
+++ cropmap/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_njqb*-c_zv$454g(h()zklda0nd!y%buun!fzlbq7@avd^3c9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cropmapapp.herokuapp.com']
 
